887240781cbf2878bbb30efd08d967acfb67be91
Tanner Hoelzel <tannerhoelzel@Tanners-MacBook-Pro-3.local>
2017-02-23 01:39:53 -0500

    Saves and updates log for each url

--- verify.py
+++ verify.py
@@ -1,28 +1,42 @@
 import urllib2
 import hashlib
+from time import gmtime, strftime
 
 def verify():
-  urlsfile = open('urls.txt', 'r')
-  hashesfile = open('hashes.txt', 'r+')
+  # Open URLs
+  urlsfile = open('data/urls.txt','r')
   urls = urlsfile.readlines()
-  hashes = hashesfile.readlines()
   hasher = hashlib.md5()
   for idx, url in enumerate(urls):
+    url = url.strip()
+    # Open URL log
+    try:
+      urlfile = open('data/'+url.replace('/',u'\u2215'),'r+')
+    except IOError:
+      urlfile = open('data/'+url.replace('/',u'\u2215'),'w+')
+    changes = urlfile.readlines()
+    # Check URL
     response = urllib2.urlopen(url)
     html = response.read()
+    print html
     hasher.update(html)
     hexhash = hasher.hexdigest()
-    if idx > len(hashes)-1:
-      print 'new'
-      hashes.append(hexhash + '\n')
-    elif hexhash == hashes[idx].strip():
-      print 'same'
+    # Append URL log
+    if len(changes) == 0:
+      changes.append(strftime("%Y-%m-%d %H:%M:%S START\n", gmtime()))
+      changes.append(hexhash+'\n')
+    elif hexhash == changes[-1].strip():
+      changes[-1] = strftime("%Y-%m-%d %H:%M:%S\n", gmtime())
+      changes.append(hexhash+'\n')
     else:
-      print 'diff'
-      hashes[idx] = hexhash + '\n'
-    hashesfile.seek(0)
-    hashesfile.truncate()
-    hashesfile.writelines(hashes)
+      changes[-1] = strftime("%Y-%m-%d %H:%M:%S CHANGE\n", gmtime())
+      changes.append(hexhash+'\n')
+    # Close URL log
+    urlfile.seek(0)
+    urlfile.truncate()
+    urlfile.writelines(changes)
+    urlfile.close()
+  urlsfile.close()
   
 if __name__ == "__main__":
 	verify()
